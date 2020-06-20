5fb319145f7555dd320b2a926ec1ab045c761a15
guochengqian <gordonqian2017@gmail.com>
2020-06-21 00:31:53 +0300

    add detailed commendts on prepare_s3dis_data.py

--- data_conversions/prepare_s3dis_data.py
+++ data_conversions/prepare_s3dis_data.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-'''Prepare Data for S3DIS Segmentation Task.'''
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -17,9 +15,18 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import data_utils
 
 
+"""
+This .py file reads the xyzrgb.npy from the folder. 
+shift xyz in each room to the center 
+normalize rgb from -0.5 to 0.5
+
+
+"""
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--folder', '-f', help='Path to data folder')
+    parser.add_argument('--folder', '-f', help='Path to data folder', default='/data/3D/s3dis_aligned/prepare_label_rgb')
     parser.add_argument('--max_point_num', '-m', help='Max point number of each sample', type=int, default=8192)
     parser.add_argument('--block_size', '-b', help='Block size', type=float, default=1.5)
     parser.add_argument('--grid_size', '-g', help='Grid size', type=float, default=0.03)
@@ -28,7 +35,7 @@ def main():
     args = parser.parse_args()
     print(args)
 
-    root = args.folder if args.folder else '../../data/s3dis'
+    root = args.folder
     max_point_num = args.max_point_num
 
     batch_size = 2048
@@ -38,19 +45,20 @@ def main():
     label_seg = np.zeros((batch_size, max_point_num), dtype=np.int32)
     indices_split_to_full = np.zeros((batch_size, max_point_num), dtype=np.int32)
 
-    for area_idx in range(1, 7):
+    # for each area
+    for area_idx in range(2, 7):    # todo: 1, 7
         folder = os.path.join(root, 'Area_%d' % area_idx)
-        datasets = [dataset for dataset in os.listdir(folder)]
-        for dataset_idx, dataset in enumerate(datasets):
-            dataset_marker = os.path.join(folder, dataset, ".dataset")
-            if os.path.exists(dataset_marker):
-                print('{}-{}/{} already processed, skipping'.format(datetime.now(), folder, dataset))
+        rooms = [room for room in os.listdir(folder)] # rooms
+        for room_idx, room in enumerate(rooms):
+            room_marker = os.path.join(folder, room, ".room")
+            if os.path.exists(room_marker):
+                print('{}-{}/{} already processed, skipping'.format(datetime.now(), folder, room))
                 continue
-            filename_data = os.path.join(folder, dataset, 'xyzrgb.npy')
+            filename_data = os.path.join(folder, room, 'xyzrgb.npy')
             print('{}-Loading {}...'.format(datetime.now(), filename_data))
             xyzrgb = np.load(filename_data)
 
-            filename_labels = os.path.join(folder, dataset, 'label.npy')
+            filename_labels = os.path.join(folder, room, 'label.npy')
             print('{}-Loading {}...'.format(datetime.now(), filename_labels))
             labels = np.load(filename_labels).astype(int).flatten()
 
@@ -71,13 +79,17 @@ def main():
                 xyz_min = np.amin(xyz, axis=0, keepdims=True) - offset
                 xyz_max = np.amax(xyz, axis=0, keepdims=True)
                 block_size = (args.block_size, args.block_size, 2 * (xyz_max[0, -1] - xyz_min[0, -1]))
+                # todo: why x2 in z.
+
+                # collect pillars.
                 xyz_blocks = np.floor((xyz - xyz_min) / block_size).astype(np.int)
 
                 print('{}-Collecting points belong to each block...'.format(datetime.now(), xyzrgb.shape[0]))
+                # blocks is the splited blocks. point_block_indices is the index of block for each point
                 blocks, point_block_indices, block_point_counts = np.unique(xyz_blocks, return_inverse=True,
                                                                             return_counts=True, axis=0)
                 block_point_indices = np.split(np.argsort(point_block_indices), np.cumsum(block_point_counts[:-1]))
-                print('{}-{} is split into {} blocks.'.format(datetime.now(), dataset, blocks.shape[0]))
+                print('{}-{} is split into {} blocks.'.format(datetime.now(), room, blocks.shape[0]))
 
                 block_to_block_idx_map = dict()
                 for block_idx in range(blocks.shape[0]):
@@ -117,11 +129,14 @@ def main():
 
                 # uniformly sample each block
                 for block_idx in range(idx_last_non_empty_block + 1):
+                    # get the points of each pillar.
                     point_indices = block_point_indices[block_idx]
                     if point_indices.shape[0] == 0:
                         continue
                     block_points = xyz[point_indices]
                     block_min = np.amin(block_points, axis=0, keepdims=True)
+
+                    # uniformly sample using grids
                     xyz_grids = np.floor((block_points - block_min) / args.grid_size).astype(np.int)
                     grids, point_grid_indices, grid_point_counts = np.unique(xyz_grids, return_inverse=True,
                                                                              return_counts=True, axis=0)
@@ -165,14 +180,14 @@ def main():
                         idx_in_batch = idx % batch_size
                         data[idx_in_batch, 0:point_num, ...] = block_xzyrgb[start:end, :]
                         data_num[idx_in_batch] = point_num
-                        label[idx_in_batch] = dataset_idx  # won't be used...
+                        label[idx_in_batch] = room_idx  # won't be used...
                         label_seg[idx_in_batch, 0:point_num] = block_labels[start:end]
                         indices_split_to_full[idx_in_batch, 0:point_num] = point_indices[start:end]
 
                         if ((idx + 1) % batch_size == 0) or \
                                 (block_idx == idx_last_non_empty_block and block_split_idx == block_split_num - 1):
                             item_num = idx_in_batch + 1
-                            filename_h5 = os.path.join(folder, dataset, '%s_%d.h5' % (offset_name, idx_h5))
+                            filename_h5 = os.path.join(folder, room, '%s_%d.h5' % (offset_name, idx_h5))
                             print('{}-Saving {}...'.format(datetime.now(), filename_h5))
 
                             file = h5py.File(filename_h5, 'w')
@@ -185,13 +200,13 @@ def main():
 
                             if args.save_ply:
                                 print('{}-Saving ply of {}...'.format(datetime.now(), filename_h5))
-                                filepath_label_ply = os.path.join(folder, dataset, 'ply_label',
+                                filepath_label_ply = os.path.join(folder, room, 'ply_label',
                                                                   'label_%s_%d' % (offset_name, idx_h5))
                                 data_utils.save_ply_property_batch(data[0:item_num, :, 0:3],
                                                                    label_seg[0:item_num, ...],
                                                                    filepath_label_ply, data_num[0:item_num, ...], 14)
 
-                                filepath_rgb_ply = os.path.join(folder, dataset, 'ply_rgb',
+                                filepath_rgb_ply = os.path.join(folder, room, 'ply_rgb',
                                                                 'rgb_%s_%d' % (offset_name, idx_h5))
                                 data_utils.save_ply_color_batch(data[0:item_num, :, 0:3],
                                                                 (data[0:item_num, :, 3:] + 0.5) * 255,
@@ -200,8 +215,9 @@ def main():
                             idx_h5 = idx_h5 + 1
                         idx = idx + 1
 
-            # Marker indicating we've processed this dataset
-            open(dataset_marker, "w").close()
+            # Marker indicating we've processed this room
+            open(room_marker, "w").close()
+
 
 if __name__ == '__main__':
     main()
